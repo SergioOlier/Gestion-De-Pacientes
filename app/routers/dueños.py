e865b7bd692f62e5de8f6292265eabe67873a239
SergioOlier <solierm17@gmail.com>
2024-10-04 06:26:59 -0500

    ADD: Implementa rutas para gestionar dueños

--- app/routers/dueños.py
+++ app/routers/dueños.py
@@ -1,1 +1,23 @@
 #Las rutas para manejar las peticiones relacionadas a dueños
+
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from .. import crud, schemas
+from ..database import get_db
+
+router = APIRouter()
+
+@router.post("/", response_model=schemas.Dueño)
+def create_dueño(dueño: schemas.DueñoCreate, db: Session = Depends(get_db)):
+    return crud.create_dueño(db=db, dueño=dueño)
+
+@router.get("/", response_model=list[schemas.Dueño])
+def read_dueños(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
+    return crud.get_dueños(db, skip=skip, limit=limit)
+
+@router.get("/{dueño_id}", response_model=schemas.Dueño)
+def read_dueño(dueño_id: int, db: Session = Depends(get_db)):
+    db_dueño = crud.get_dueño(db, dueño_id)
+    if db_dueño is None:
+        raise HTTPException(status_code=404, detail="Dueño no encontrado")
+    return db_dueño
