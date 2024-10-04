c3b99de609dddbe803cd75d626cd684b41d753df
SergioOlier <solierm17@gmail.com>
2024-10-04 06:13:55 -0500

    ADD: Implementacion de funciones CRUD para manejar operaciones de mascotas en la base de datos

--- app/crud.py
+++ app/crud.py
@@ -1,1 +1,16 @@
 #Aquí irán las funciones CRUD para interactuar con la base de datos.
+
+from sqlalchemy.orm import Session
+from . import models, schemas
+
+# Función para crear una mascota
+def create_mascota(db: Session, mascota: schemas.MascotaCreate):
+    db_mascota = models.Mascota(**mascota.dict())
+    db.add(db_mascota)
+    db.commit()
+    db.refresh(db_mascota)
+    return db_mascota
+
+# Función para obtener una lista de mascotas
+def get_mascotas(db: Session, skip: int = 0, limit: int = 10):
+    return db.query(models.Mascota).offset(skip).limit(limit).all()
